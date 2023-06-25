1573c97467a7d9c62f89064962324cfe9f274cc6
Joseph <ralphlsulit@gmail.com>
2023-06-26 07:01:39 +0800

    adding success page

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const email = document.querySelector('.email');
 const form = document.querySelector('.form');
+const userEmail = document.querySelector('.user-email');
 const errorElement = document.querySelector('.error');
 
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
   let messages = [];
+  let emailValue = email.value;
 
-  if (email.value === '' || email.value === null) {
+  if (emailValue === '' || emailValue === null) {
     messages.push('Email is required');
     email.style.borderColor = 'hsl(4, 100%, 67%)';
-  } else if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+  } else if (!emailValue.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
     messages.push('Valid email required');
     email.style.color = 'hsl(4, 100%, 67%)';
     email.style.borderColor = 'hsl(4, 100%, 67%)';
     email.style.backgroundColor = 'hsl(4.8,100%,95.1%)';
+  } else {
+    window.location.href = './success-page/success-page.html?email=' + encodeURIComponent(emailValue);
   }
   
   if (messages.length > 0) {
-    e.preventDefault();
     errorElement.innerText = messages.join(', ');
   }
 });
